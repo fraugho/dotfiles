1a66b7dd1e3c7e6bea8aeceeaf4ee704e483f36d
fraugho <144178952+fraugho@users.noreply.github.com>
2023-10-11 01:43:01 -0800

    Create launch.sh

---
+++ polybar-compact/launch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Detect the current window manager and launch the appropriate Polybar configuration
+if pgrep -x "i3" >/dev/null; then
+    polybar mainbar-i3 &
+elif pgrep -x "openbox" >/dev/null; then
+    polybar mainbar-openbox &
+elif pgrep -x "bspwm" >/dev/null; then
+    polybar mainbar-bspwm &
+elif pgrep -x "herbstluftwm" >/dev/null; then
+    polybar mainbar-herbstluftwm &
+# ... add more conditions for other window managers as needed
+else
+    echo "No recognized window manager is running!"
+    exit 1
+fi
+
+echo "Polybar launched..."
